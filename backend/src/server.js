cbc1534c417331a43f658de19cd49297305bf473
Yeshi Dorji <yeshijrod99@gmail.com>
2025-10-15 13:30:52 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,21 +24,7 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// });
-
 app.use("/api/notes", notesRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
