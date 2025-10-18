a8e1feeeb626731a34e6463c0fb34c7ce55ce61e
Yeshi Dorji <yeshijrod99@gmail.com>
2025-10-18 23:26:00 +0600

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await api.get("https://note-app-2wvg.onrender.com/");
+        const res = await api.get("https://note-app-2wvg.onrender.com/notes");
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
